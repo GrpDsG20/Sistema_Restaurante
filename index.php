e1a93b05128c6748b494c95ad7cdadbc32e97af6
Smith <59782720+GrpDsG20@users.noreply.github.com>
2025-01-29 23:33:19 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>	
+	<title>El secreto de Don Lucho</title>
+	<link rel="stylesheet" href="./style.css">
+
+
+<h1 style="text-align:center;">Restaurante <br> El secreto de Don Lucho</h1>
+
+
+	<a href="staff" >Empleado</a>
+	<a href="admin">Administrador</a>
+
+</head>
+<body>
+	<style type="text/css">
+		
+		html{			
+			background: url(./image/rest.jpg);
+
+		}	
+		html::after {
+  		content:"";
+  		position:absolute;
+ 			top:0;
+	 	  left:0;
+	 	  width:100%;
+	 	  height:100%;
+ 		  background:rgba(0,0,0,0.5);
+ 		  z-index:-1;
+		}		
+
+		body{
+			background: none;
+			color: white;
+			margin-top: 100px;
+		}
+		a{
+			background: #EF5635;
+		}
+
+
+	</style>
+
+	
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+	
+</body>
+</html>
